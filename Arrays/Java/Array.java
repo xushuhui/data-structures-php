548c19413587038e992f6bbe0a70104fb9f5e634
xushuhui <474497097@qq.com>
2019-07-05 14:00:13 +0800

    update

---
+++ Arrays/Java/Array.java
@@ -0,0 +1,52 @@
+public class Array {
+    private int[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0
+    }
+
+    public Array() {
+        this(10)
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length
+    }
+
+    public boolean isEmpty() {
+        return size == 0
+    }
+
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("AddLast failed. Array is full.");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("AddLast failed. Array is full.");
+        }
+        for (int i = size - 1; i >= index,i--){
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++
+    }
+
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    @Override
+    public String toString(){
+    }
+
+}
