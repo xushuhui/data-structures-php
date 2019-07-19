94cb57724b303b1154fc2581de7241a5b88ad6bb
xushuhui <474497097@qq.com>
2019-07-19 13:57:06 +0800

    update

--- LinkedList/LinkedList.php
+++ LinkedList/LinkedList.php
@@ -8,10 +8,11 @@
  */
 include_once "Node.php";
 class LinkedList{
-    private $head;
+   
     private $size;
+    private $dummyHead;
     public function __construct() {
-        $this->head = null;
+        $this->dummyHead = new Node(null,null);
         $this->size = 0;
     }
     public function getSize():int
@@ -22,15 +23,20 @@ class LinkedList{
     {
         return $this->size == 0;
     }
-    public function addFirst($e){
-        $this->add(0,$e);
-        $this->head = new Node($e, $this->head);
-        $this->size ++;
-    }
+  
     public function add($index,$e){
         if($index <0 || $index > $this->size){
             throw new Exception("index is illegal");
         }
+        $prev = $this->dummyHead;
+        for ($i=0; $i < $index; $i++) { 
+            $prev = $prev->next;
+        }
+        $prev->next = new Node($e,$prev->next);
+        $this->size ++;
+    }
+    public function addFirst($e){
+        $this->add(0,$e);
     }
     public function addLast($e){
         $this->add($this->size,$e);
@@ -39,14 +45,31 @@ class LinkedList{
         if($index <0 || $index > $this->size){
             throw new Exception("index is illegal");
         }
+        $cur = $this->dummyHead->next;
+        for ($i=0; $i < $index; $i++) { 
+            $cur = $cur->next;
+        }
+        $cur->e = $e;
     }
     public function contains($e){
-        
+        $cur = $this->dummyHead->next;
+        while($cur != null){
+            if ($cur->e == $e){
+                return true;
+            }
+            $cur = $cur->next;
+        }
+        return false;
     }
     public function get($index){
         if($index <0 || $index > $this->size){
             throw new Exception("index is illegal");
         }
+        $cur = $this->dummyHead->next;
+        for ($i=0; $i < $index; $i++) { 
+            $cur = $cur->next;
+        }
+        return $cur->e;
     }
     public function getFirst(){
         $this->get(0);
